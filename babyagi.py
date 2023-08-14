75647590b8dcc96b0c44a053f21ec930fdc3caef
William04567 <100785107+William04567@users.noreply.github.com>
2023-08-14 21:02:00 +0200

    Update babyagi.py

--- babyagi.py
+++ babyagi.py
@@ -365,7 +365,8 @@ def openai_call(
                     frequency_penalty=0,
                     presence_penalty=0,
                 )
-                return response.choices[0].text.strip()
+                #return response.choices[0].text.strip()
+                return response
             else:
                 # Use 4000 instead of the real limit (4097) to give a bit of wiggle room for the encoding of roles.
                 # TODO: different limits for different models.
@@ -382,7 +383,96 @@ def openai_call(
                     n=1,
                     stop=None,
                 )
-                return response.choices[0].message.content.strip()
+                #return response.choices[0].message.content.strip()
+                return response
+        except openai.error.RateLimitError:
+            print(
+                "   *** The OpenAI API rate limit has been exceeded. Waiting 10 seconds and trying again. ***"
+            )
+            time.sleep(10)  # Wait 10 seconds and try again
+        except openai.error.Timeout:
+            print(
+                "   *** OpenAI API timeout occurred. Waiting 10 seconds and trying again. ***"
+            )
+            time.sleep(10)  # Wait 10 seconds and try again
+        except openai.error.APIError:
+            print(
+                "   *** OpenAI API error occurred. Waiting 10 seconds and trying again. ***"
+            )
+            time.sleep(10)  # Wait 10 seconds and try again
+        except openai.error.APIConnectionError:
+            print(
+                "   *** OpenAI API connection error occurred. Check your network settings, proxy configuration, SSL certificates, or firewall rules. Waiting 10 seconds and trying again. ***"
+            )
+            time.sleep(10)  # Wait 10 seconds and try again
+        except openai.error.InvalidRequestError:
+            print(
+                "   *** OpenAI API invalid request. Check the documentation for the specific API method you are calling and make sure you are sending valid and complete parameters. Waiting 10 seconds and trying again. ***"
+            )
+            time.sleep(10)  # Wait 10 seconds and try again
+        except openai.error.ServiceUnavailableError:
+            print(
+                "   *** OpenAI API service unavailable. Waiting 10 seconds and trying again. ***"
+            )
+            time.sleep(10)  # Wait 10 seconds and try again
+        else:
+            break
+
+
+def old_openai_call(
+    prompt: str,
+    model: str = LLM_MODEL,
+    temperature: float = OPENAI_TEMPERATURE,
+    max_tokens: int = 100,
+):
+    while True:
+        try:
+            if model.lower().startswith("llama"):
+                result = llm(prompt[:CTX_MAX],
+                             stop=["### Human"],
+                             echo=False,
+                             temperature=0.2,
+                             top_k=40,
+                             top_p=0.95,
+                             repeat_penalty=1.05,
+                             max_tokens=200)
+                # print('\n*****RESULT JSON DUMP*****\n')
+                # print(json.dumps(result))
+                # print('\n')
+                return result['choices'][0]['text'].strip()
+            elif model.lower().startswith("human"):
+                return user_input_await(prompt)
+            elif not model.lower().startswith("gpt-"):
+                # Use completion API
+                response = openai.Completion.create(
+                    engine=model,
+                    prompt=prompt,
+                    temperature=temperature,
+                    max_tokens=max_tokens,
+                    top_p=1,
+                    frequency_penalty=0,
+                    presence_penalty=0,
+                )
+                #return response.choices[0].text.strip()
+                return response
+            else:
+                # Use 4000 instead of the real limit (4097) to give a bit of wiggle room for the encoding of roles.
+                # TODO: different limits for different models.
+
+                trimmed_prompt = limit_tokens_from_string(prompt, model, 4000 - max_tokens)
+
+                # Use chat completion API
+                messages = [{"role": "system", "content": trimmed_prompt}]
+                response = openai.ChatCompletion.create(
+                    model=model,
+                    messages=messages,
+                    temperature=temperature,
+                    max_tokens=max_tokens,
+                    n=1,
+                    stop=None,
+                )
+                #return response.choices[0].message.content.strip()
+                return response
         except openai.error.RateLimitError:
             print(
                 "   *** The OpenAI API rate limit has been exceeded. Waiting 10 seconds and trying again. ***"
